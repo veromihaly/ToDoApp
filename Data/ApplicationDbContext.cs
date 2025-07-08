ece630addff9baabdd0245d5edce4a4808c7d0f8
Verő_Mihály <mrdttyb480222@mailbox.unideb.hu>
2025-07-08 12:33:17 +0200

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ToDoApp.Models;
+
+namespace ToDoApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoTask> TodoTasks { get; set; }
+    }
+}
---
+++ Models/ToDoTask.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ToDoApp.Models
+{
+    public class TodoTask
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        public string? Description { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime? DueDate { get; set; }
+
+        public bool IsCompleted { get; set; }
+
+        
+        /* public string UserId { get; set; }
+
+        [ForeignKey("UserId")]
+        public ApplicationUser User { get; set; } */
+    }
+}
